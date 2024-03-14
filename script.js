604543e314056e82f362dedc112d4b6222d3ecbd
Ian Jones <ianjones@Work-MBP.local>
2024-03-13 22:29:42 -0600

    Added modal form for book input

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const myLibrary = []
+let bookModal = document.getElementById("bookModal");
+let bookButton = document.querySelector(".book-button");
+let span = document.querySelector(".close");
 
 //Book constructor
-function book(title, author, pages, genre, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.genre = genre;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, genre, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.genre = genre;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary() {
 
+}
+
+
+bookButton.onclick = function() {
+    bookModal.style.display = "block";
+}
+
+span.onclick = function() {
+    bookModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        bookModal.style.display = "none";
+    }
 }
