c4dada97588196f564f2d12f4cd8cb261c94faf6
Ian Jones <ianjones@Work-MBP.local>
2024-03-13 20:12:10 -0600

    Added html and css styling

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = []
+
+//Book constructor
+function book(title, author, pages, genre, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.genre = genre;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
